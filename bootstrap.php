ad0bae2c4aa8739160b88656e6dc0e0571b4fda4
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-01-25 03:10:05 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,40 +1,28 @@
-<?php
+<?php namespace Organizer;
 
-function root(?string $path = null): ?string {
-    static $root;
-    if (isset($path)) {
-        if (!\is_dir($path))
-            throw new \InvalidArgumentException("$path is not a directory");
+// Get the Composer autoload file if it exists
+$composer_autoloader = null;
 
-        $_root = $root;
-        $root = \realpath($path) . DIRECTORY_SEPARATOR;
-        return $_root;
-    }
-
-    if (!isset($root))
-        throw new \RuntimeException("root() is not set");
-
-    return $root;
-}
-
-function organize(string $root): array {
-    root($root);
-    $organizer_data = [];
-    if (\is_readable($organizer_file = root() . 'organizer.json')) {
-        $organizer_data = \json_decode(
-            \file_get_contents($organizer_file),
-            true
-        );
-
-        if (\json_last_error() > 0) {
-            \fprintf(
-                STDERR,
-                '%s in %s' . PHP_EOL,
-                \json_last_error_msg(),
-                $organizer_file
-            );
-            exit(1);
+$root = __DIR__;
+while (!isset($composer_autoloader) && dirname($root) !== $root) {
+    if (is_file($composer_file = $root . DIRECTORY_SEPARATOR . 'composer.json') && is_readable($composer_file)) {
+        if (($composer_data = @file_get_contents($composer_file)) && ($composer_data = @json_decode($composer_data, true))) {
+            if (is_file($autoload_file = $root . DIRECTORY_SEPARATOR . ($composer_data['config']['vendor-dir'] ?? 'vendor') . DIRECTORY_SEPARATOR . 'autoload.php') && is_readable($autoload_file)) {
+                $composer_autoloader = require_once $autoload_file;
+            }
+            unset($autoload_file);
         }
+        unset($composer_data);
     }
-    return $organizer_data;
+    unset($composer_file);
+    $root = dirname($root);
 }
+unset($root);
+
+// Get Organizer
+$organizer_autoloader = require_once __DIR__ . '/src/core/autoload.php';
+spl_autoload_register($organizer_autoloader);
+$organizer = new Manager(__DIR__, true, $composer_autoloader);
+spl_autoload_unregister($organizer_autoloader);
+unset($organizer_autoloader, $composer_autoloader);
+return $organizer;
